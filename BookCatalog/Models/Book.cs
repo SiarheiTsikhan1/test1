ddba47ba0991372a19a8b2ddb4e758049f30e5c5
Gefgs <gefgs2@mail.ru>
2022-10-05 12:04:59 +0200

    first commit

---
+++ BookCatalog/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCatalog.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        [Range(1, 10, ErrorMessage = "Rating must be between 1 and 10")]
+        public int Range { get; set; }
+    }
+}
